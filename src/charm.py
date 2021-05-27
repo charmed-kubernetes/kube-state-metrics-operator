852185d2588e1d8ef190c95bd8bb1e3bce8712b9
Cory Johns <johnsca@gmail.com>
2021-05-27 14:37:27 -0400

    Remove some boilerplate, add supported config option and basic unit test

--- src/charm.py
+++ src/charm.py
@@ -17,24 +17,26 @@ import logging
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus
 
 logger = logging.getLogger(__name__)
 
 
-class KubeStateMetricsCharm(CharmBase):
+class KubeStateMetricsOperator(CharmBase):
     """Charm the service."""
 
     _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+        self.framework.observe(
+            self.on.kube_state_metrics_pebble_ready,
+            self._on_kube_state_metrics_pebble_ready,
+        )
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
         self._stored.set_default(things=[])
 
-    def _on_httpbin_pebble_ready(self, event):
+    def _on_kube_state_metrics_pebble_ready(self, event):
         """Define and start a workload using the Pebble API.
 
         TEMPLATE-TODO: change this example to suit your needs.
@@ -48,20 +50,20 @@ class KubeStateMetricsCharm(CharmBase):
         container = event.workload
         # Define an initial Pebble layer configuration
         pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
+            "summary": "kube-state-metrics layer",
+            "description": "pebble config layer for kube-state-metrics",
             "services": {
-                "httpbin": {
+                "kube-state-metrics": {
                     "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
+                    "summary": "kube-state-metrics",
+                    "command": "gunicorn -b 0.0.0.0:80 kube-state-metrics:app -k gevent",
                     "startup": "enabled",
                     "environment": {"thing": self.model.config["thing"]},
                 }
             },
         }
         # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
+        container.add_layer("kube-state-metrics", pebble_layer, combine=True)
         # Autostart any services that were defined with startup: enabled
         container.autostart()
         # Learn more about statuses in the SDK docs:
@@ -69,36 +71,14 @@ class KubeStateMetricsCharm(CharmBase):
         self.unit.status = ActiveStatus()
 
     def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
-
-        Learn more about config at https://juju.is/docs/sdk/config
-        """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
-
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
+        """Enforce valid config and update service if needed."""
+        if self.config["metric-allowlist"] and self.config["metric-denylist"]:
+            self.unit.status = BlockedStatus(
+                "metric-allowlist and metric-denylist are mutually exclusive"
+            )
         else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+            self.unit.status = ActiveStatus()
 
 
 if __name__ == "__main__":
-    main(KubeStateMetricsCharm)
+    main(KubeStateMetricsOperator)
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,35 +1,39 @@
-# Copyright {{ year }} {{ author }}
+# Copyright 2021 Cory Johns
 # See LICENSE file for licensing details.
 
-import unittest
-from unittest.mock import Mock
-
+import pytest
+from ops.model import ActiveStatus, BlockedStatus
 from ops.testing import Harness
 from charm import KubeStateMetricsOperator
 
 
-class TestCharm(unittest.TestCase):
-    def test_config_changed(self):
-        harness = Harness(KubeStateMetricsOperator)
-        self.addCleanup(harness.cleanup)
-        harness.begin()
-        self.assertEqual(list(harness.charm._stored.things), [])
-        harness.update_config({"thing": "foo"})
-        self.assertEqual(list(harness.charm._stored.things), ["foo"])
+@pytest.fixture
+def harness():
+    harness = Harness(KubeStateMetricsOperator)
+    try:
+        yield harness
+    finally:
+        harness.cleanup()
 
-    def test_action(self):
-        harness = Harness(KubeStateMetricsOperator)
-        harness.begin()
-        # the harness doesn't (yet!) help much with actions themselves
-        action_event = Mock(params={"fail": ""})
-        harness.charm._on_fortune_action(action_event)
 
-        self.assertTrue(action_event.set_results.called)
+def test_config_changed(harness):
+    harness.begin()
 
-    def test_action_fail(self):
-        harness = Harness(KubeStateMetricsOperator)
-        harness.begin()
-        action_event = Mock(params={"fail": "fail this"})
-        harness.charm._on_fortune_action(action_event)
+    # confirm valid config is accepted
+    harness.update_config(
+        {
+            "metric-allowlist": "foo",
+            "metric-labels-allowlist": "foo",
+            "namespaces": "foo",
+            "resources": "foo",
+        }
+    )
+    assert isinstance(harness.charm.unit.status, ActiveStatus)
 
-        self.assertEqual(action_event.fail.call_args, [("fail this",)])
+    # confirm conflicting config is blocked
+    harness.update_config(
+        {
+            "metric-denylist": "foo",
+        }
+    )
+    assert isinstance(harness.charm.unit.status, BlockedStatus)
