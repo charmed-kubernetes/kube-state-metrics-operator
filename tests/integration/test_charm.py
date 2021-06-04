243affd0097356a426981f9ce102072be6f57736
Cory Johns <johnsca@gmail.com>
2021-06-04 17:12:28 -0400

    Refactor test slightly and switch to fork of Prometheus charm for bug fix

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 import pytest
@@ -7,26 +8,30 @@ log = logging.getLogger(__name__)
 
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test, helpers):
+    ksm_charm = await ops_test.build_charm(".")
+    # NB: We have to use the repo for Prometheus for now to get the updated
+    # sidecar version.
+    prometheus_charm = await ops_test.build_charm(
+        await helpers.fetch_charm_src_from_github(
+            ops_test.tmp_path,
+            # Temporarily use branch on fork until
+            # https://github.com/canonical/prometheus-operator/pull/40 lands.
+            "johnsca/prometheus-operator",
+            branch="gh/39/eschew-update-status",
+            # "canonical/prometheus-operator",
+            # branch="master",
+        )
+    )
+
+    # NB: We can't use a bundle for now due to https://github.com/juju/python-libjuju/issues/506
     await ops_test.model.deploy(
-        await ops_test.build_charm("."),
+        ksm_charm,
         resources={
             "kube-state-metrics-image": "k8s.gcr.io/kube-state-metrics/kube-state-metrics:v2.0.0"
         },
     )
-    await ops_test.model.wait_for_idle(wait_for_active=True)
-
-    # Add Prometheus (this is done separately so that we can at least verify
-    # above that the kube-state-metrics charm successfully builds & deploys,
-    # even if the Prometheus charm doesn't). NB: We have to use the repo for
-    # now to get the updated sidecar version.
-    prometheus_charm = await helpers.fetch_charm_src_from_github(
-        ops_test.tmp_path,
-        "canonical/prometheus-operator",
-        branch="master",
-    )
-
     await ops_test.model.deploy(
-        await ops_test.build_charm(prometheus_charm),
+        prometheus_charm,
         resources={
             "prometheus-image": "prom/prometheus",
         },
@@ -36,9 +41,19 @@ async def test_build_and_deploy(ops_test, helpers):
 
 
 async def test_stats_in_prometheus(ops_test, helpers):
-    result = await helpers.query_prometheus(
-        ops_test, 'count(kube_pod_status_phase{phase="Running"} > 0)'
-    )
-    assert result["status"] == "success"
-    assert result["data"]["result"] != []
-    assert int(result["data"]["result"][0]["value"][1]) > 0
+    result = None
+    for attempt in range(3):
+        try:
+            result = await helpers.query_prometheus(
+                ops_test, 'count(kube_pod_status_phase{phase="Running"} > 0)'
+            )
+        except AssertionError:
+            # Prometheus might not be up yet
+            pass
+        else:
+            assert result["status"] == "success"
+            if result["data"]["result"]:
+                break
+        await asyncio.sleep(5)  # the scrape interval is 5s
+    else:
+        pytest.fail(f"Failed to get expected query result: {result}")
